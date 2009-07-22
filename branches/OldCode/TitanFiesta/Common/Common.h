ce17718b9c0efc92217f5b6793336fead8663f54
Drakia <Drakia@20a06461-0953-0410-b9d3-43271db211d1>
2009-07-22 21:11:58 +0000

    Changed F_TIME, it now has a 6-bit day instead of always having the 6th bit 0.

--- branches/OldCode/TitanFiesta/Common/Common.h
+++ branches/OldCode/TitanFiesta/Common/Common.h
@@ -6,10 +6,9 @@ const int SERVERTYPE = USASERVER; // 0 = USA Server / 1 = EUR Server
 struct f_date {
 	unsigned year : 8;
 	unsigned month : 5;
-	unsigned day : 5;
-	unsigned nul: 1;
+	unsigned day : 6;
 	unsigned hour : 6;
 	unsigned minute : 7;
 
-	f_date(){year=0;month=0;day=0;nul=0;hour=0;minute=0;};
+	f_date(){year=0;month=0;day=0;hour=0;minute=0;};
 };
