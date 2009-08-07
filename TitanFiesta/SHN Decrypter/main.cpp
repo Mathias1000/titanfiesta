7888e8d212a52d693c289e6cd1f5374af3caa38a
Drakia <Drakia@20a06461-0953-0410-b9d3-43271db211d1>
2009-08-07 04:56:27 +0000

    Updated SHN handler, fixed the decryption algorithm. Login/Char servers now properly empty serverlist on ISC serverlist packet.

--- TitanFiesta/SHN Decrypter/main.cpp
+++ TitanFiesta/SHN Decrypter/main.cpp
@@ -165,6 +165,18 @@ private:
 	std::vector<SSHNColumn*> columns;
 };
 
+unsigned char* ShnDecrypt(unsigned char* data, long int filesize) {
+	byte key = filesize & 0xFF;
+
+	for(long int i = filesize - 1; i > 0; --i){
+		data[i] ^= key;
+		byte tmp1 = ( 0x0b * (byte)i );
+		byte tmp2 = ( i & 0x0f ) + 0x55;
+		key ^= (tmp1 ^ tmp2) ^ 0xaa;
+	}
+	return data;
+}
+
 int main(int argc, char** argv){
 	if(argc < 3){
 		return 0;
@@ -182,33 +194,7 @@ int main(int argc, char** argv){
 	fread(data, filesize, 1, fh);
 	fclose(fh);
 
-	data += 0x24;
-	filesize -= 0x24;
-	unsigned char esp10 = data[filesize - 1];
-	unsigned char dl;
-	unsigned int eax = filesize;
-	for(long int i = filesize - 1; i > 0; --i){
-		dl = data[i];
-		dl ^= (eax & 0xFF);
-		eax &= ~0xFF;
-		eax |= i & 0xFF;
-		data[i] = dl;
-		char tmp = eax & 0xff;
-		short tmp2 = tmp * 0x0b;
-		eax &= ~0xFFFF;
-		eax |= *reinterpret_cast<unsigned short*>(&tmp2);
-		dl = i & 0xff;
-		dl &= 0x0f;
-		dl += 0x55;
-		dl ^= eax & 0xFF;
-		dl ^= esp10;
-		dl ^= 0xaa;
-		eax &= ~0xFF;
-		eax |= dl & 0xFF;
-		esp10 = eax & 0xFF;
-	}
-	data -= 0x24;
-	filesize += 0x24;
+	ShnDecrypt(data + 0x24, filesize - 0x24);
 
 	{
 		char tempPath[256];
